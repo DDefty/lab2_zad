8dc1f5311c99eb8625ff3da2d488608c5426931f
Dawid <dawcio9914@gmail.com>
2021-04-01 16:20:51 +0200

    getMails impl

--- app.js
+++ app.js
@@ -1,40 +1,23 @@
 const _ = require('lodash');
 
-const user = {
-    name: 'Imie',
-    surname: 'Nazwisko',
-    allGrades:[
-       {
-       subjectName: 'Name1',
-       grades: [5, 4, 3, 5, 2],
-       weight: 3
-       },
-       {
-       subjectName: 'Name2',
-       grades: [3, 3.5, 2],
-       weight: 1
-       },
-       {
-       subjectName: 'Name3',
-       grades: [4, 3, 3.5],
-       weight: 5
-       }
-    ]
+const collections = [
+    {},
+    15,
+    "Hello@test.pl",
+    null,
+    ['aaa','bbb',5],
+    'admin@gmail.com',
+    undefined,
+    'a34@yahoo.com',
+    '321@a',
+    '321.pl'
+];
+function getMails(){
+    const reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    let sorted = [];
+    sorted = _.filter(collections, function (entry) {
+      return typeof entry === "string" && reg.test(entry)
+    });
+    sorted.sort();
+    console.log(sorted);
 }
-function average(){
-  const { allGrades } = user;
-
-let sum = 0;
-let average = 0;
-let wage = 0;
-for(let i = 0; i < allGrades.length; i++) {
-  for(let j = 0; j < allGrades[i].grades.length; j++) {
-    sum += allGrades[i].grades[j] * allGrades[i].weight;
-  }
-  wage += allGrades[i].grades.length * allGrades[i].weight;
-}
-average = sum / wage;
-console.log(average.toFixed(2));
-}
-const { allGrades } = user;
-console.log(_.find(allGrades, ['weight', 1] ));
